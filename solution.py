830b33d4714f89b9c5f32413b12ba5bdbc6d062f
jf4600 <98861587+jf4600@users.noreply.github.com>
2022-02-13 17:02:39 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -33,7 +33,8 @@ def webServer(port=13331):
 
                 # Send one HTTP header line into socket.
                 #connectionSocket.send("HTTP/1.1 200\r\n\r\n".encode())
-                connectionSocket.send(bytes("HTTP/1.1 200\r\n\r\n", "utf-8".encode))
+                #connectionSocket.send(bytes("HTTP/1.1 200\r\n\r\n", "utf-8".encode))
+                connectionSocket.send("HTTP/1.1 200 OK ")
                 # Fill in start
 
                 # Fill in end
